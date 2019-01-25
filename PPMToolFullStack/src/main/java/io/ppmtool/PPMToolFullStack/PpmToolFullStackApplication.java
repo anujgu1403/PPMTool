02b0cd98c8979bd84d989c277df1310d5666b13c
Anuj Kumar <anujgupta1403@gmail.com>
2019-01-25 16:52:26 +0530

    Curd implemented

---
+++ PPMToolFullStack/src/main/java/io/ppmtool/PPMToolFullStack/PpmToolFullStackApplication.java
@@ -0,0 +1,14 @@
+package io.ppmtool.PPMToolFullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolFullStackApplication.class, args);
+	}
+
+}
+

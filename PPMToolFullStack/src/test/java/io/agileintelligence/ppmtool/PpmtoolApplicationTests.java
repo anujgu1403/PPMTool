e25a01541582b90e27cd61e8a31648843f5dee7f
ctscoderepo <anujgupta1403@gmail.com>
2019-04-10 12:30:51 +0530

    Project changed

--- PPMToolFullStack/src/test/java/io/agileintelligence/ppmtool/PpmtoolApplicationTests.java
+++ PPMToolFullStack/src/test/java/io/agileintelligence/ppmtool/PpmtoolApplicationTests.java
@@ -1,4 +1,4 @@
-package io.ppmtool.PPMToolFullStack;
+package io.agileintelligence.ppmtool;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,11 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PpmToolFullStackApplicationTests {
+public class PpmtoolApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
-
